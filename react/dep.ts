13241100356c5f6fb76fc93e295e2d55bf01b3c2
ScriptMaster <msheriffusa@gmail.com>
2020-09-07 13:33:13 +0400

    Create dep.ts

---
+++ react/dep.ts
@@ -0,0 +1,7 @@
+// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+import React from "https://dev.jspm.io/react@16.13.1"
+
+// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+export { default as React } from "https://dev.jspm.io/react@16.13.1";
+export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1/server";
+export { opine } from "https://deno.land/x/opine@0.21.2/mod.ts";
